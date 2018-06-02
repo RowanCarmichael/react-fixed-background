651e466adb446f0c229459b68b0c110d8444652f
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2018-06-02 18:17:59 +1200

    ejecting

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          require.resolve('sass-loader')
+        ],
+      },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+    ],
+  },
+  externals: {
+    react: 'commonjs react',
+  },
+};

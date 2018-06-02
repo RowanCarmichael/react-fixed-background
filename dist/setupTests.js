f0740160ca0326845e9e48de12ecfc1b7955500a
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2018-06-02 19:27:10 +1200

    Updating build script

--- dist/setupTests.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-global.React = require('react');
-var Enzyme = require('enzyme');
-var EnzymeAdapter = require('enzyme-adapter-react-16');
-
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });

469be36ffb2b7e8fd268df8bd2e3ec8ec403bc25
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2018-06-02 16:04:06 +1200

    Adding HorizonalScrollLayout export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Container from './components/container/Container';
 import Background from './components/background/Background';
 import Content from './components/content/Content';
+import HorizontalScrollLayout from './components/horizontalScrollLayout/HorizontalScrollLayout';
 
-export { Container, Background, Content };
+export { Container, Background, Content, HorizontalScrollLayout };
